66abf48bdd1a127ccb5e03a360210038cb775c04
Craowx <112517491+Craowx@users.noreply.github.com>
2024-11-01 19:24:03 +0100

    Added functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+const messages = [ //Messages array
+                    "Those who don't believe in magic will never find it.", 
+                    'Light tomorrow with today.', 
+                    'Lead from the heart, not the head.', 
+                    'You cannot change what you are, only what you do.',
+                    'Hope is the most exciting thing there is in life.',
+                    'Say something positive, and you’ll see something positive.',
+                    'Every strike brings me closer to the next home run.',
+                    'The happiness of your life depends on the quality of your thoughts.',
+                    'The power of imagination makes us infinite.',
+                    'Anything is possible with sunshine and a little pink.'
+                ];
+
+console.log(messages[Math.floor(Math.random() * 10)]); //Randomize number and multiply by 10 to access random index
